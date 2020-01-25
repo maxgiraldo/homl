abb6eac293ba8b3ef15f60b17ca40675c4f0553e
Maximilian A. Giraldo <maxagiraldo@gmail.com>
2020-01-25 09:40:41 -0500

    chore: add more aliases

--- aliases.sh
+++ aliases.sh
@@ -1,1 +1,4 @@
-alias mlrun='docker run -v $HOME/code/ml/homl/notebooks:/home/jovyan/work/notebooks -p 8888:8888 homl_notebook'
+alias mlrun='docker run -v $HOME/code/ml/homl/notebooks:/home/jovyan/work/notebooks -p 8888:8888 homl'
+alias mlbuild='docker build -t homl .'
+alias rmcontainers='docker rm $(docker ps -a -q)'
+alias stopcontainers='docker stop $(docker ps -a -q)'
